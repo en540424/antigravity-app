e89a2b138ca2af95f332f0264458ec94debd24ba
en540424 <info@e-nexus.trade>
2025-12-02 20:01:22 +0900

    fix company page

---
+++ app/company/page.js
@@ -0,0 +1,8 @@
+export default function CompanyPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>E-NEXUS Company Information</h1>
+      <p>This page will contain company details.</p>
+    </div>
+  );
+}
