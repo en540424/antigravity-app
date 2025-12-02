166cf2a348b21660d9db54036c51556dc7451d3f
en540424 <info@e-nexus.trade>
2025-12-02 23:56:02 +0900

    fix supabase import

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
+
 export async function POST(req) {
   try {
     const formData = await req.formData();
