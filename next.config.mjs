f17f05d6551fb5b38cab6bc5d2627fad089b5623
en540424 <info@e-nexus.trade>
2025-12-03 00:58:15 +0900

    fix env config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
